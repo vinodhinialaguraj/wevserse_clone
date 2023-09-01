dac29840a8caad3ee77a3d7ad35ab70d287cbdd6
vinodhinialaguraj <vinodhini.alaguraj@gmail.com>
2023-09-01 18:04:56 +0530

    first demo ver

--- weverse/src/App.js
+++ weverse/src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import Image from './components/image';
 import Signup from './components/signup';
 import Account from './components/account'*/
-import Home  from './src/components/home'
-import Mainpage from './src/components/mainpage';
-import Content from './src/components/content';
-import Artistimages from './src/components/artistpage'
+import Home  from './components/home'
+import Mainpage from './components/mainpage';
+import Content from './components/content';
+import Artistimages from './components/artistpage'
 import { Route, Routes } from 'react-router-dom';
 //import PreviewMultipleImages from './components/PreviewMultipleImages';
 function App() {
